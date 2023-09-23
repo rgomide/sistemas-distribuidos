6053bae35f26b1e351a1f60e655a764f05e189af
rgomide <renato.s.gomide@gmail.com>
2023-09-23 00:50:15 -0300

    .

--- rest/src/routes/middlewares/log.js
+++ rest/src/routes/middlewares/log.js
@@ -1,4 +1,4 @@
-module.exports = (req, res, next) => {
+module.exports = (req, _res, next) => {
   console.log('REQUEST')
   console.log(req.method, req.originalUrl)
   console.log('params =>', req.params)
