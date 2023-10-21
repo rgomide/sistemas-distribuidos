1b2bc4388dd0e247d8376c13bedb04f3ecc9dd80
rgomide <renato.s.gomide@gmail.com>
2023-10-20 22:09:41 -0300

    .

--- kafka/producer/src/producer.js
+++ kafka/producer/src/producer.js
@@ -3,12 +3,17 @@ const kafka = require('./config/kafka-connector')
 const main = async () => {
   const producer = kafka.producer()
 
+  const mensagem = {
+    type: 'imovel-insert',
+    id: 1
+  }
+
+  const mensagemJson = JSON.stringify(mensagem)
+
   await producer.connect()
   await producer.send({
     topic: 'meu-topico',
-    messages: [
-      { value: 'Hello KafkaJS user!' },
-    ],
+    messages: [{ value: mensagemJson }]
   })
 
   await producer.disconnect()
