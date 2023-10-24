8d16bbcef475a87c10bbcb537b4f6e6983074aaa
rgomide <renato.s.gomide@gmail.com>
2023-10-24 10:46:15 -0300

    .

--- java-threads/src/main/java/gomide/HeavyProcess.java
+++ java-threads/src/main/java/gomide/HeavyProcess.java
@@ -7,12 +7,16 @@ public class HeavyProcess {
   public static void justDoIt(int id) throws InterruptedException {
     System.out.println(String.format("Processo iniciado para id: %d", id));
 
+    long tempoInicio = System.currentTimeMillis();
+
     Random random = new Random();
     int milisegundos = random.nextInt(3000);
 
     Thread.sleep(milisegundos);
 
-    System.out.println(String.format("Processo finalizado para id: %d em %dms", id, milisegundos));
+    long tempoFim = System.currentTimeMillis() - tempoInicio;
+
+    System.out.println(String.format("Processo finalizado para id: %d em %dms", id, tempoFim));
   }
 
 }
