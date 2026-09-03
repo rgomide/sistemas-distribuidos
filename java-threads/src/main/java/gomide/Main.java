82dffac19da976d03c1e506deeea42da13ded03d
rgomide <renato.s.gomide@gmail.com>
2026-09-03 17:04:31 -0300

    .

--- java-threads/src/main/java/gomide/Main.java
+++ java-threads/src/main/java/gomide/Main.java
@@ -6,7 +6,7 @@ public class Main {
     System.out.println("INÍCIO");
     long tempoInicio = System.currentTimeMillis();
 
-    for (int i = 0; i < 20; i++) {
+    for (int i = 0; i < 10; i++) {
       HeavyProcess.justDoIt(i);
     }
 
